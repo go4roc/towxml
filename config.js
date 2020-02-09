0c24b37add92d4c61d4b14ab4362dd3eb3ec921b
Rock <ferlysky@gmail.com>
2020-02-09 15:35:38 +0800

    mini

--- config.js
+++ config.js
@@ -2,14 +2,14 @@ module.exports = {
     // LaTex公式、yuml解析服务架设参见 https://github.com/sbfkcel/markdown-server
 
     // 数学公式解析API
-    latex:{
-        api:'http://towxml.vvadd.com/?tex'
-    },
+    // latex:{
+    //     api:'http://towxml.vvadd.com/?tex'
+    // },
 
     // yuml图解析APPI
-    yuml:{
-        api:'http://towxml.vvadd.com/?yuml'
-    },
+    // yuml:{
+    //     api:'http://towxml.vvadd.com/?yuml'
+    // },
 
     // markdown解析配置，保留需要的选项即可
     markdown:[
@@ -18,20 +18,20 @@ module.exports = {
         'ins',                      // 文本删除线支持
         'mark',                     // 文本高亮支持
         'emoji',                    // emoji表情支持
-        'todo'                      // todo支持
+        // 'todo'                      // todo支持
     ],
 
     // 代码高亮配置，保留需要的选项即可
     highlight:[
-        'bash',
-        'javascript',
-        'json',
-        'python',
-        'htmlbars',
-        'css',
-        'php',
-        'scss',
-        'shell',
+        // 'bash',
+        // 'javascript',
+        // 'json',
+        // 'python',
+        // 'htmlbars',
+        // 'css',
+        // 'php',
+        // 'scss',
+        // 'shell',
 
         // 'coffeescript',
         // 'dns',
@@ -89,11 +89,11 @@ module.exports = {
     // 自定义组件
     components:[
         'audio-player',             // 音频组件，建议保留，由于小程序原生audio存在诸多问题，towxml解决了原生音频播放器的相关问题
-        'echarts',                  // echarts图表支持
-        'latex',                    // 数学公式支持
-        'table',                    // 表格支持
-        'todogroup',                // todo支持
-        'yuml',                     // yuml图表支持
+        // 'echarts',                  // echarts图表支持
+        // 'latex',                    // 数学公式支持
+        // 'table',                    // 表格支持
+        // 'todogroup',                // todo支持
+        // 'yuml',                     // yuml图表支持
         'img'                       // 图片解析组件
     ],
 
@@ -106,7 +106,7 @@ module.exports = {
     ],
 
     // 事件绑定方式（catch或bind），catch 会阻止事件向上冒泡。更多请参考：https://developers.weixin.qq.com/miniprogram/dev/framework/view/wxml/event.html
-    bindType:'catch',
+    // bindType:'catch',
 
     // 需要激活的事件
     events:[
@@ -114,13 +114,13 @@ module.exports = {
         // 'touchmove',
         // 'touchcancel',
         // 'touchend',
-        'tap',                      // 用于元素的点击事件
-        'change',                   // 用于todoList的change事件
+        // 'tap',                      // 用于元素的点击事件
+        // 'change',                   // 用于todoList的change事件
     ],
 
     // 图片倍数
     dpr:1,
 
     // 代码块显示行号
-    showLineNumber:true
+    // showLineNumber:true
 }
